d464baa0a092935ced055c5e045e92d5242b5f78
Isaac Greenspan <isaac.greenspan@vokalinteractive.com>
2015-01-06 12:15:51 -0600

    Removing boilerplate from Example-OSX app delegate.

--- Example-OSX/VOKBenkode-Example/AppDelegate.m
+++ Example-OSX/VOKBenkode-Example/AppDelegate.m
@@ -9,18 +9,9 @@
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
-
 @property (weak) IBOutlet NSWindow *window;
 @end
 
 @implementation AppDelegate
-
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
-}
-
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
-}
 
 @end
