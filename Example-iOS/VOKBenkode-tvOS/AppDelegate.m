eb9667877efb1f72770f32fdb3f7102649440240
Carl Hill-Popper <chillpop@gmail.com>
2016-04-12 17:00:45 -0500

    Add tvOS target that runs the same unit tests

---
+++ Example-iOS/VOKBenkode-tvOS/AppDelegate.m
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.m
+//  VOKBenkode-tvOS
+//
+//  Created by Carl Hill-Popper on 4/12/16.
+//  Copyright © 2016 VOKAL Interactive. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+@end
---
+++ Example-iOS/VOKBenkode-tvOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VOKBenkode-tvOS
+//
+//  Created by Carl Hill-Popper on 4/12/16.
+//  Copyright © 2016 VOKAL Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
